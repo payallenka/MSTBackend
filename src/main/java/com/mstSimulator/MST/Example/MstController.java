3471b759800849ed88940f66a3098d5ed24ea509
Payal Lenka <payalm.lenka@gmail.com>
2024-09-29 02:21:13 +0530

    Update MstController.java

--- src/main/java/com/mstSimulator/MST/Example/MstController.java
+++ src/main/java/com/mstSimulator/MST/Example/MstController.java
@@ -1,20 +1,15 @@
 package com.mstSimulator.MST.Example;
 
-
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class MstController {
 
-    @GetMapping
+    @GetMapping("/")
     public String home() {
         return "Welcome to the MST API!";
     }
 }
-
-
-
